0614f07a45767ad276894ac6c0733015b16f53ec
Naresh-Manthrabuddi <naresh.manthrabuddi@appsassociates.com>
2017-05-12 17:39:27 +0530

    Add files via upload

---
+++ InspecExample/TomactKitchen/test/integration/default/controls/operating_system_spec.rb
@@ -0,0 +1,15 @@
+control 'operating_system' do
+
+describe port(8080) do
+  it { should be_listening }
+end
+
+  describe service('tomcat8') do
+   it { should be_enabled }
+   it { should be_running }
+  end
+
+describe file('/var/lib/tomcat8/webapps/TestRestWebservice.war') do
+it { should exist }
+end
+end
---
+++ InspecExample/TomactKitchen/testing.tfvars
@@ -0,0 +1,6 @@
+access_key = "AKIAJ44KCP6LKERUKFXQ"
+secret_key = "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"
+key_name = "SaltStack"
+region = "us-east-2"
+ami = "ami-618fab04"
+instance_type = "t2.micro"
